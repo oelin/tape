dd8bb492ad0618162ffb2c4c2ab184ea06f07c44
kale <42823429+oelin@users.noreply.github.com>
2023-06-29 10:42:34 +0100

    Create tape.py

---
+++ tape.py
@@ -0,0 +1,11 @@
+class Tape:
+
+  def parameter(self, x):
+    if isinstance(self.tape, Iterator):
+      return next(self.tape)
+
+    return self.tape.add(x) or x
+
+  def run(self, f, x, tape):
+    self.tape = iter(tape) if tape else tape
+    return f(x), tape
